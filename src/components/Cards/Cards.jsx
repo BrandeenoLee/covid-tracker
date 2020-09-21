362c15f6d71e5e155695d68151c39cb84829574b
BrandeenoLee <brandonladcock@gmail.com>
2020-09-21 17:01:07 -0500

    updates

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,55 +1,45 @@
 import React from 'react';
-import { Card, cardcontent, Typography, Grid, StylesProvider } from '@material-ui/core';
-import CountUp from 'react-CountUp';
-import cx from 'classnames';
+import { Grid } from '@material-ui/core';
+import CardComponent from './Cards';
 
 import styles from './Cards.module.css';
 
-const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate } }) => {
-    if(!confirmed) {
-        return 'Loading...';
-    }
+const Info = ({data}) => {
+ console.log('Info componentloading...',data)
+ if (!data) 
+  return "Loading..";
+
+const { confirmed, recovered, deaths, lastUpdate } = data;
+if (!confirmed) 
+  return "Fetching...";
 
 
-    return (
-        <div className={styles.container}>
-            <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography varaint="h5"> 
-                        <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
-                        </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography varaint="h5"> 
-                        <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
-                        </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of recoveries from COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                        <Typography varaint="h5"> 
-                        <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
-                        </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
-                    </CardContent>
-                    </Grid>
-                    </Grid>
-
-
-
- </div>
-    )
-}
-
-export default Cards;
+return (
+  <div className={styles.container}>
+    <Grid container spacing={3} justify="center">
+      <CardComponent
+        className={styles.infected}
+        cardTitle="Infected"
+        value={confirmed.value}
+        lastUpdate={lastUpdate}
+        cardSubtitle="Number of active cases from COVID-19."
+      />
+      <CardComponent
+        className={styles.recovered}
+        cardTitle="Recovered"
+        value={recovered.value}
+        lastUpdate={lastUpdate}
+        cardSubtitle="Number of recoveries from COVID-19."
+      />
+      <CardComponent
+        className={styles.deaths}
+        cardTitle="Deaths"
+        value={deaths.value}
+        lastUpdate={lastUpdate}
+        cardSubtitle="Number of deaths caused by COVID-19."
+      />
+    </Grid>
+  </div>
+);
+};
+export default Info;
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
 
+import { fetchCountries } from '../../api';
+
 import styles from './CountryPicker.module.css';
 
-const CountryPicker = () => {
-    return (
-        <FormControl className={StylesProvider.formControl}>
-            <NativeSelect>
-                <option value="global>Global</option>"
-            </NativeSelect>
-        </FormControl>
-    )
-}
+const Countries = ({ handleCountryChange }) => {
+  const [countries, setCountries] = useState([]);
 
-export default CountryPicker;
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, []);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+        <option value="">Global</option>
+        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default Countries;
